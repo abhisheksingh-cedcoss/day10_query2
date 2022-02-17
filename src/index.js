0aac6837f36b18cb38d289268cc10488774f43fa
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-17 13:52:05 +0530

    all commits done

--- src/index.js
+++ src/index.js
@@ -1,102 +1,115 @@
-var products = [];
+var productArr = [];
+
+$(document).ready(function(){
+
+    $("#addition").click(function(){
+        add();
+    });
+
+    $("#display_table").click(function(){
+        display();
+    });
+
+    $("#edit").click(function(){
+      $("#addition").hide()
+      $("#delete").hide()
+        edit();
+    });
+
+    $("#delete").click(function(){
+      Delete();
+    });
+
+    $("#update").click(function(){
+      $("#edit").hide()
+      $("#delete").show()
+        update();
+    });
 
 
-$(document).ready(function () {
+});
 
+function add(){
 
-  $("#addition").click(function () {
+  var id = document.getElementById("id").value;
+  var name = document.getElementById("fname").value;
+  var price = document.getElementById("price").value;
+  var quantity = document.getElementById("quantity").value;
 
-    var id = document.getElementById("id").value;
-    var name = document.getElementById("fname").value;
-    var price = document.getElementById("price").value;
-    var quan = document.getElementById("quantity").value;
-
-    var product = {
+  var obj = {
       id: id,
       name: name,
       price: price,
-      quantity: quan,
-    };
-
-    products.push(product);
-
-    console.log(products);
-
-    console.log(display(products))
- });
-$("#delete").click(function(){
-   var x = document.getElementById("id").value;
-    for(var i=0; i<products.length; i++){
-        alert("show")
-        if(products[i].id == x){
-            alert("sure to delete")
-            products.splice(i,1);
-        }
-    }
-   
-})
+      quantity: quantity
+  };
 
 
-function display(products){
-    var html = "<table> <tr><td>ID</td> <td>Name</td> <td>Price</td> <td>quantity</td>"
+  productArr.push(obj);
 
-    for(var i=0; i<products.length; i++){
-        html +=
-        "<tr><td>" +
-        products[i].id +
-        "</td><td>" +
-        products[i].name +
-        "</td><td>" +
-        "$" +
-        products[i].price +
-        "</td><td>" +
-        products[i].quantity +
-        '</td>';
+  console.log(productArr);
+}
+
+function edit(){
+  var e_dit = document.getElementById("id").value;
+  for(var i =0; i<productArr.length; i++){
+    if(productArr[i]== e_dit){
+      $('#fname').val(productArr[i].name);
+      $('#price').val(productArr[i].price);
+			$('#quantity').val(productArr[i].quantity);
 
     }
-    html += "</table>";
-
-    document.getElementById("products_list").innerHTML=html;
-
+  }
+}
 
 
+function update(){
+  var u_id =document.getElementById("id").value;
+	// console.log(u_id);
+	for (var i = 0; i < productArr.length; i++) {
+		if (productArr[i].id == u_id) {
+			productArr[i].name = document.getElementById('fname').value;
+			productArr[i].price = document.getElementById('price').value;
+			productArr[i].quantity = document.getElementById('quantity').value;
+			// console.log(productArr[i].name);
+			// console.log(productArr[i].price);
+		}
+	}
 
+  // display();
+}
+
+
+
+
+function Delete() {
+	var d_id = document.getElementById("id").value;
+	for (var i = 0; i < productArr.length; i++) {
+		if (productArr[i].id == d_id) {
+            if (confirm('Are you sure you want to delete'+ productArr[i].id +' row')) {
+                   productArr.splice(i,1)
+            }
+		}
+	}
+}
+
+function display(){
+  var dis = '<table><tr><th>Id</th><th>Name</th><th>Price</th><th>Quantity</th></tr>';
+	for (var i = 0; i < productArr.length; i++) {
+		dis +=
+			'<tr><td>' +
+			productArr[i].id +
+			'</td><td>' +
+			productArr[i].name +
+			'</td><td>' +
+			productArr[i].price +
+			'</td><td>' +
+			productArr[i].quantity +
+			'</td></tr>';
+	}
+
+	dis += '</table>';
+	document.getElementById('display').innerHTML = dis;
 
 
 
 }
-
-$("display_table").click(function(){
-})
-
-$("#update_product").click(function () {
-    var v_id = document.getElementById("id").value;
-    var v_name = document.getElementById("fname").value;
-    var v_price = document.getElementById("price").value;
-    var v_quantity = document.getElementById("quantity").value;
-  
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].id == v_id) {
-        products[i].id = v_id;
-        products[i].name = v_name;
-        products[i].price = v_price;
-        products[i].quantity = v_quantity;
-      }
-    }
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
